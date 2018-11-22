81d6522996d8c6f87beebfb9bc8fe27ea25d3366
Simon Honigmann <simonhonigmann@gmail.com>
2018-11-22 01:06:40 +0100

    late night working

--- Lab 7/Lab7.m
+++ Lab 7/Lab7.m
@@ -63,93 +63,154 @@ n_gps = [sigma_n*n_gpsN,sigma_e*n_gpsE];
 gps = x_m+n_gps;
 
 %% Kalman Filter Setup
-x(:,1) = [0,0,0,0]'; % pn, pe, vn, ve
-xtild = x;
-xhat = x;
-
-%uncertainty of inital measurement
-sigma_x = 10;
-sigma_v = 0.05;
-sigma_xn = sigma_x;
-sigma_vn = sigma_v;
-sigma_xe = sigma_x;
-sigma_ve = sigma_v;
-
-z = zeros(4,1);%randn(1,4).*[sigma_x,sigma_x, sigma_v,sigma_v]; %initial measurement, with white noise uncertainty
-
-F(1,:,:) = [0 0 1 0; 0 0 0 1; 0 0 0 0; 0 0 0 0]; %dynamic matrix
-H = [1 0 0 0; 0 1 0 0]; %measurement design matrix
-G = [0 0; 0 0; 1 0; 0 1]; %noise shaping matrix
-
-R = [sigma_xn 0 0 0; ...
-     0 sigma_xe 0 0; ...
-     0 0 sigma_vn 0;...
-     0 0 0 sigma_ve]; %covariance of measurement noise
-
-%NOT SURE ABOUT THIS????
-dq_vn = 0.05;
-dq_ve = 0.05;
 dt = 2; % time step used in prediction
+
+%initialize x, z, F, H, and G
+x = zeros(4,n); % pn, pe, vn, ve
+x(:,1) = [x_m(1,:)';dx_m(1,:)'];% assume initial state is known
+
+xtild = 0*x;%assume no initial state knowledge
+%xhat = 0*x;%assume no initial state knowledge
+xhat = x;%assume perfect initial state knowledge
+z = gps'; %measurements, with white noise uncertainty
+
+syms deltat;
+Adt = [0 0 deltat 0; 0 0 0 deltat; 0 0 0 0; 0 0 0 0];
+F=subs(expm(Adt),deltat,dt);%dynamic matrix calculated using matrix exponential
+
+H = [1 0 0 0; 0 1 0 0]; %measurement design matrix
+
+%noise shaping matrix
+%G = [dt*eye(2);eye(2)]; %not actually used here since Qk is pre-derived
+
+%Create R
+sigma_x = 0.5;
+R = sigma_x*eye(2); %covariance of measurement noise
+
+% Solve for Qk using derivation from lecture
+dq_v = 0.05; %sigma_vdot
+dq_vn = dq_v;
+dq_ve = dq_v;
+
 Qk(1,:,:) = [1/3*dt^3*dq_vn, 0 ,1/2*dt^2*dq_vn, 0;...
       0, 1/3*dt^3*dq_ve, 0 ,1/2*dt^2*dq_ve; ...
       1/2*dt^2*dq_vn, 0,dt*dq_vn, 0; ...
       0, 1/2*dt^2*dq_ve, 0, dt*dq_ve]; %covariance matrix of the system noise
 
-P = ; %covariance of state estimate
+%Define P - uncertainty of inital measurement 
+sigma_x_state = 10; 
+sigma_v_state = 0.1; 
+P(1,:,:) = diag([sigma_x_state,sigma_x_state,sigma_v_state,sigma_v_state]).^2; %covariance of state estimate
 %Phat = P; % a posteriori covariance of state estimate (after measurement)
 Ptild = P; % a priori covariance of state estimate (before measurement)
 
-I = eye(size(P));
+I = eye(size(squeeze(P(1,:,:))));
   
 %% Kalman Filter Loop
 %try different prediction deltaT and update deltaT
-for k=1:n
+k=1;
+while k<n
+%for k=1:n-1
    
     %prediction:
-    xtild(:,k+1)=squeeze(F(k,:,:))*xhat(:,k);
-    Ptild(k+1,:,:) = squeeze(F(k,:,:))*P(k,:,:)*squeeze(F(k,:,:))'+Qk(k,:,:);
+    xtild(:,k+1)=F*xhat(:,k);
+    Ptild(k+1,:,:) = F*squeeze(P(k,:,:))*F'+squeeze(Qk(1,:,:));
+    
+    k=k+1;
     
     %calculate kalman gain
-    %%CHECK IF Ptild SHOULD USE K or K+1 HERE!!!!!!!!!!!!!! EG WHEN DOES
-    %%TIME UPDATE ACTUALLY HAPPEN
-    K(k,:,:) = Ptild(k,:,:)*H(k,:,:)*(H(k,:,:)*Ptild(k,:,:)*H(k,:,:)+R(k,:,:));
+    K(k,:,:) = squeeze(Ptild(k,:,:))*H'/(H*squeeze(Ptild(k,:,:))*H'+R);
     
     %update state
-    xhat(k,:) = xtild(k,:)+K(k,:,:)*(z(k,:)-H(k,:,:)*xtild(k,:));
+    xhat(:,k) = xtild(:,k)+squeeze(K(k,:,:))*(z(:,k)-H*xtild(:,k));
     
-    %%CHECK IF Ptild SHOULD USE K or K+1 HERE!!!!!!!!!!!!!!
     %update covariance
-    P(k+1,:,:) = (I-K(k,:,:)*H(k,:,:))*Ptild(k,:,:);
-    
+    P(k,:,:) = (I-squeeze(K(k,:,:))*H)*squeeze(Ptild(k,:,:));
+   
 end
 
-%% Plotting Trajectories
+%% Extract KF State Info
+x_n_K = xhat(1,:);
+x_e_K = xhat(2,:);
+v_n_K = xhat(3,:);
+v_e_K = xhat(4,:);
+
+%% Calculations
+sigma_xy_gps_emp = sqrt(std(x_m(:,1)-gps(:,1))^2+std(x_m(:,2)-gps(:,2))^2); %4.1
+sigma_xy_KF_emp = sqrt(std(x_m(:,1)-x_n_K')^2+std(x_m(:,2)-x_e_K')^2); %4.2
+sigma_xy_KF_P = sqrt(std(squeeze(P(:,1,1)))^2+std(squeeze(P(:,2,2)))^2);
+
+innovation_all = zeros(size(gps'));
+for i=1:n
+    innovation_all(:,i) = gps(i,:)'-H*xtild(:,i); %4.3
+    %innovation_stable = ;
+end
+%plots for innovation
+figure(17);
+plot(innovation_all(1,:));
+hold on;
+plot(innovation_all(2,:));
+
+%% Calculate Errors
+gps_err = n_gps;
+kalman_err = xhat(1:2,:)-x_m';
+v_kalman_err = xhat(3:4,:)-dx_m';
+
+%% PLOTS REQUIRED: 2x1 subplot with position measurements (ref, meas, KF) and error (meas, KF)
+%%Plotting Trajectories
 figure(k);
 %position
 subplot(2,1,1);
 plot(x_m(:,2),x_m(:,1));
 hold on;
-xlabel('X_m_2, East (m)');
-ylabel('X_m_1, North (m)');
-title(['True vs Estimate Position (',method,' Integration, ',sample_rate_text,'Hz)']);
-plot(x_sd(:,2),x_sd(:,1));
+xlabel('East (m)');
+ylabel('North (m)');
+title('Trajectory Comparison of Raw GPS and Kalman Filter');
+plot(gps(:,1),gps(:,2));
+plot(x_e_K,x_n_K);
 axis('equal');
-legend('True',strcat(method,', ',sample_rate_text,'Hz'));
+legend('Ref','GPS','KF');
 
-%% Calculate Errors
+%%Plotting Errors
+subplot(2,1,2);
+scatter(gps_err(:,2),gps_err(:,1),'filled');
+hold on;
+scatter(kalman_err(2,:),kalman_err(1,:),'filled');
+xbound = max(abs([kalman_err(1,:)';gps_err(:,1)]));
+ybound = max(abs([kalman_err(2,:)';gps_err(:,2)]));
+xlim([-xbound,xbound]);
+ylim([-ybound,ybound]);
+xlabel('East Error (m)');
+ylabel('North Error (m)');
+legend('GPS','KF');
 
-%% PLOTS REQUIRED: 2x1 subplot with position measurements (ref, meas, KF) and error (meas, KF)
-%%               : 2x1 subplot with velocitz measurements (ref, KF) and error (KF)
+
+%%               : 2x1 subplot with velocity measurements (ref, KF) and error (KF)
+figure(k+1);
+%velocity
+subplot(2,1,1);
+plot(dx_m(:,2),dx_m(:,1));
+hold on;
+xlabel('East (m/s)');
+ylabel('North (m/s)');
+title('Velocity Comparison of Kalman Filter and the Reference');
+plot(v_e_K,v_n_K);
+axis('equal');
+legend('Ref','KF');
+
+%%Plotting Errors
+subplot(2,1,2);
+scatter(v_kalman_err(2,:),v_kalman_err(1,:),'filled');
+hold on;
+xbound = max(abs([v_kalman_err(1,:)';gps_err(:,1)]));
+ybound = max(abs([v_kalman_err(2,:)';gps_err(:,2)]));
+xlim([-xbound,xbound]);
+ylim([-ybound,ybound]);
+xlabel('East Error (m/s)');
+ylabel('North Error (m/s)');
+legend('KF');
+
 %%               : Histogram innovation histogram for north and east
 
-%% Plotting Errors
-%position
-subplot(2,1,2);
-plot(t,err_x(:,1));
-hold on;
-plot(t,err_x(:,2));
-plot(t,err_x(:,3));
-xlabel('Time (s)');
-ylabel('X_m Error (m)');
-title(['Position Error Over Time (',method,' Integration, ',sample_rate_text,'Hz)']);
-legend('North','East','Error Magnitude');
+
+
